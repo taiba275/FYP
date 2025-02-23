2581a397df123aa5cd666669b1fed7542d2c74bb
taiba275 <taibahanif275@gmail.com>
2025-02-23 23:58:43 +0500

    footer fix, env variable fix

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,50 +3,50 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-<footer class="bg-white text-gray-800 font-sans text-sm px-10 py-5">
-    <div class="max-w-screen-xl mx-auto flex justify-between items-center">
-        <div class="flex items-center space-x-10">
+<footer className="bg-white text-gray-800 font-sans text-sm px-10 py-5">
+    <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-10">
             <div>W.</div>
-            <div class="flex space-x-10">
-                <div class="flex flex-col space-y-3 font-bold">
-                    <a href="#" class="text-lg hover:text-gray-600">Websites</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Collections</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Elements</a>
+            <div className="flex space-x-10">
+                <div className="flex flex-col space-y-3 font-bold">
+                    <a href="#" className="text-lg hover:text-gray-600">Websites</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Collections</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Elements</a>
                 </div>
-                <div class="flex flex-col space-y-3 font-bold">
-                    <a href="#" class="text-lg hover:text-gray-600">Academy</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Jobs</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Market</a>
+                <div className="flex flex-col space-y-3 font-bold">
+                    <a href="#" className="text-lg hover:text-gray-600">Academy</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Jobs</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Market</a>
                 </div>
-                <div class="flex flex-col space-y-3 font-bold">
-                    <a href="#" class="text-lg hover:text-gray-600">Directory</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Conferences</a>
+                <div className="flex flex-col space-y-3 font-bold">
+                    <a href="#" className="text-lg hover:text-gray-600">Directory</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Conferences</a>
                 </div>
-                <div class="flex flex-col space-y-3 font-bold">
-                    <a href="#" class="text-lg hover:text-gray-600">FAQs</a>
-                    <a href="#" class="text-lg hover:text-gray-600">About Us</a>
-                    <a href="#" class="text-lg hover:text-gray-600">Contact Us</a>
+                <div className="flex flex-col space-y-3 font-bold">
+                    <a href="#" className="text-lg hover:text-gray-600">FAQs</a>
+                    <a href="#" className="text-lg hover:text-gray-600">About Us</a>
+                    <a href="#" className="text-lg hover:text-gray-600">Contact Us</a>
                 </div>
             </div>
         </div>
-        <div class="flex items-center space-x-4">
-            <a href="#" class="bg-gray-100 hover:bg-gray-200 text-sm px-4 py-2 rounded-md">Next Conferences: Berlin, Mumbai & Tokyo</a>
+        <div className="flex items-center space-x-4">
+            <a href="#" className="bg-gray-100 hover:bg-gray-200 text-sm px-4 py-2 rounded-md">Next Conferences: Berlin, Mumbai & Tokyo</a>
         </div>
     </div>
-    <div class="max-w-screen-xl mx-auto flex justify-between items-center border-t border-gray-300 mt-3 pt-3">
-        <div class="text-gray-600">
+    <div className="max-w-screen-xl mx-auto flex justify-between items-center border-t border-gray-300 mt-3 pt-3">
+        <div className="text-gray-600">
             Cookies Policy | Legal Terms | Privacy Policy
         </div>
-        <div class="flex items-center space-x-4">
-            <a href="#" class="hover:text-gray-600">Instagram</a>
-            <a href="#" class="hover:text-gray-600">LinkedIn</a>
-            <a href="#" class="hover:text-gray-600">Twitter</a>
-            <a href="#" class="hover:text-gray-600">Facebook</a>
-            <a href="#" class="hover:text-gray-600">YouTube</a>
-            <a href="#" class="hover:text-gray-600">TikTok</a>
-            <a href="#" class="hover:text-gray-600">Pinterest</a>
+        <div className="flex items-center space-x-4">
+            <a href="#" className="hover:text-gray-600">Instagram</a>
+            <a href="#" className="hover:text-gray-600">LinkedIn</a>
+            <a href="#" className="hover:text-gray-600">Twitter</a>
+            <a href="#" className="hover:text-gray-600">Facebook</a>
+            <a href="#" className="hover:text-gray-600">YouTube</a>
+            <a href="#" className="hover:text-gray-600">TikTok</a>
+            <a href="#" className="hover:text-gray-600">Pinterest</a>
         </div>
-        <div class="text-gray-600">
+        <div className="text-gray-600">
             Sponsored by Webflow
         </div>
     </div>
